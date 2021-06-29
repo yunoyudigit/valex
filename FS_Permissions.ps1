882614a3419a829516ec48d7f945964c2a2d958e
yunoyudigit <yunoyudigit@gmail.com>
2021-06-28 22:30:53 -0400

    Add files via upload

---
+++ FS_Permissions.ps1
@@ -0,0 +1,7 @@
+﻿ <#
+
+ 1 liner for getting file owner permissions for dlls
+
+ #>
+ 
+ (Get-Acl -Path "C:\Windows\System32\*.dll") | Select-Object -Property Path,Owner | Where-Object {$_.Owner -inotlike "*NT*"}
